e0363f175720b025431eb22988320c8849732113
Snake Sanders <elmarian22@gmail.com>
2026-08-29 13:35:01 +0200

    feat: add enemies

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use rand::random;
 
 // The player sprite size
 pub const PLAYER_SIZE: f32 = 64.0;
@@ -9,16 +10,21 @@ pub const PLAYER_PADDING: f32 = PLAYER_SIZE / 2.0;
 // The player speed
 pub const PLAYER_SPEED: f32 = 500.0;
 
+pub const NUMBER_OF_ENEMIE: usize = 4;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, spawn_enemies))
         .add_systems(Update, (move_player, confine_player))
         .run();
 }
 
 #[derive(Component)]
 pub struct Player;
+
+#[derive(Component)]
+pub struct Enemy;
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let ball_img = asset_server.load("sprites/ball_blue_large.png");
@@ -64,23 +70,45 @@ pub fn confine_player(mut player: Single<&mut Transform, With<Player>>, window: 
 
     let mut translation = player.translation;
 
-    info!("{} {} w: {}", translation.x, translation.y, window.height());
-
     if translation.y + PLAYER_PADDING > window_top {
         translation.y = window_top - PLAYER_PADDING;
-        warn!("touched ceiling");
     } else if translation.y - PLAYER_PADDING < window_bottom {
         translation.y = window_bottom + PLAYER_PADDING;
-        warn!("touched ground");
     }
 
     if translation.x + PLAYER_PADDING > window_right {
-        warn!("touched right");
         translation.x = window_right - PLAYER_PADDING;
     } else if translation.x - PLAYER_PADDING < window_left {
-        warn!("touched left");
         translation.x = window_left + PLAYER_PADDING;
     }
 
     player.translation = translation;
+}
+
+pub fn spawn_enemies(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    window: Single<&Window>,
+) {
+    let enemy_img = asset_server.load("sprites/ball_red_large.png");
+    let half_width = window.width() / 2.0;
+    let half_height = window.height() / 2.0;
+
+    (0..NUMBER_OF_ENEMIE).for_each(|i| {
+        // 0.0 is at the center of the window, consider the negative areas.
+        let x: f32 = random::<f32>() * half_width * gen_sign();
+        let y: f32 = random::<f32>() * half_height * gen_sign();
+
+        commands.spawn((
+            Sprite::from_image(enemy_img.clone()),
+            Transform::from_xyz(x, y, 0.0),
+            Enemy,
+        ));
+
+        info!("Spawning enemy {i}");
+    });
+}
+
+fn gen_sign() -> f32 {
+    if rand::random::<bool>() { 1.0 } else { -1.0 }
 }
