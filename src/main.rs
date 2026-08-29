bda031a1661ac77c192d14bd22b32bc869c28571
Snake Sanders <elmarian22@gmail.com>
2026-08-29 10:17:17 +0200

    feat: show blue ball on window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn(Sprite::from_image(
+        asset_server.load("sprites/ball_blue_large.png"),
+    ));
 }
