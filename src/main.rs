7237dfb03ff28976a59c77d069fb1e098de8fd00
Snake Sanders <elmarian22@gmail.com>
2026-08-29 10:47:39 +0200

    feat: log key press events

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use bevy::prelude::*;
+use bevy::{
+    input::keyboard::{self, Key},
+    prelude::*,
+};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, move_player)
         .run();
 }
 
@@ -15,4 +19,10 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Sprite::from_image(
         asset_server.load("sprites/ball_blue_large.png"),
     ));
+}
+
+pub fn move_player(keyboard: Res<ButtonInput<KeyCode>>, key_input: Res<ButtonInput<Key>>) {
+    if keyboard.pressed(KeyCode::KeyW) {
+        info!("'w' is pressed")
+    }
 }
